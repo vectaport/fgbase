27893ae01201cd483bb1f7d931a7fb64555956cc
Scott Johnston <johnston@vectaport.com>
2015-03-20 15:49:36 -0500

    ConstNode

---
+++ constnode.go
@@ -0,0 +1,27 @@
+package flowgraph
+
+import (
+)
+
+func ConstNode(x Edge) {
+
+
+	node:=MakeNode("const", nil, []*Edge{&x}, nil)
+
+	for {
+
+		if node.Rdy() {
+			node.PrintVals()
+			node.Printf("writing x.Data: %d\n", x.Val.(int))
+			x.Data <- x.Val
+			x.Rdy = false
+		}
+
+		node.Printf("select\n")
+		select {
+		case x.Rdy = <-x.Ack:
+			node.Printf("x.Ack read\n")
+		}
+	}
+	
+}
