42999d129f8330d32a84c20c135bcd126c588392
Scott Johnston <johnston@vectaport.com>
2015-04-05 11:19:31 -0700

    doc tweak

--- func_pass.go
+++ func_pass.go
@@ -3,7 +3,7 @@ package flowgraph
 import (
 )
 
-// FuncPass sends a value two ways (x = a)
+// FuncPass passes a value on (x = a)
 func FuncPass(a, x Edge) {
 
 	node := MakeNode("pass", []*Edge{&a}, []*Edge{&x}, nil, func(n *Node) {n.Dsts[0].Val = n.Srcs[0].Val} )
