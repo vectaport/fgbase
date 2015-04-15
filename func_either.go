2b1755f99373bc8ca8f436ab42b0ef490db9e405
Scott Johnston <johnston@vectaport.com>
2015-04-15 15:47:39 -0500

    New either func

---
+++ func_either.go
@@ -0,0 +1,29 @@
+package flowgraph
+
+import (
+)
+
+func eitherFire (n *Node) {
+	a := n.Srcs[0]
+	b := n.Srcs[1]
+	x := n.Dsts[0]
+	if a.Rdy {
+		x.Val = a.Val
+		b.NoOut = true
+	} else {
+		x.Val = b.Val
+		a.NoOut = true
+	}
+}
+
+func eitherRdy (n *Node) bool {
+	return (n.Srcs[0].Rdy || n.Srcs[1].Rdy) && n.Dsts[0].Rdy
+}
+
+// FuncEither passes on one of two values (if =a { x = a } else { x = b }).
+func FuncEither(a, b, x Edge) {
+
+	node := MakeNode("either", []*Edge{&a, &b}, []*Edge{&x}, eitherRdy, eitherFire)
+	node.Run()
+
+}
