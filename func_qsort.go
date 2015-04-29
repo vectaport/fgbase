6d9cd01aa31dd1e78b354d1f6176836ff58d121b
Scott Johnston <johnston@vectaport.com>
2015-04-29 16:04:35 -0700

    working pool of qsort
    out of order too

--- func_qsort.go
+++ func_qsort.go
@@ -8,16 +8,32 @@ type Interface2 interface {
 	Sorted() bool
 }
 
+func (n *Node) sortSz(l int, ch string) {
+	if TraceLevel>=VVV {
+		var s string
+		for i :=0; i<(1024*1024); i += (1024*1024)/10 {
+			if i<l {
+				s += ch
+			} else {
+				break
+			}
+		}
+		n.Tracef("%s\n", s)
+	}
+}
 
 func qsortFire (n *Node) {
 	a := n.Srcs[0]
 	x := n.Dsts[0]
+	a.SendAck(n) // write early to let flow go on
+	a.NoOut = true
 	if _,ok := a.Val.(Interface2); !ok {
 		n.Errorf("not of type Interface2 (%T)\n", a.Val)
 		return
 	}
 
 	l := Len(a.Val)
+	n.sortSz(l, ">")
 	d := a.Val.(Interface2)
 	if l <= 1024 {
 		Sort(d)
@@ -35,6 +51,7 @@ func qsortFire (n *Node) {
 	quickSort(d, 0, mlo, maxDepth)
 	quickSort(d, mhi, l, maxDepth)
 	x.Val = x.AckWrap(d)
+	n.sortSz(l, "<")
 }
 
 // FuncQsort recursively implements a quicksort with goroutines (x=qsort(a)).
