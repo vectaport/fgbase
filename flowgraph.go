1444520d5698ff44b70114fcf922c031e78b38ef
Scott Johnston <johnston@vectaport.com>
2015-07-18 13:26:53 -0700

    doc work

--- flowgraph.go
+++ flowgraph.go
@@ -113,7 +113,7 @@ func ConfigByFlag(defaults map[string]interface{}) {
 	ChannelSize = *chanszPtr
 }
 
-// StartTime is when the flowgraph started running.
+// When the flowgraph started running.
 var StartTime time.Time
 
 // TimeSinceStart returns time since start of running flowgraph.
