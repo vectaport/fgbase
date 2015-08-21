fc71a0a205f56e092134d14389cd63c8170883de
Scott Johnston <johnston@vectaport.com>
2015-08-21 18:20:22 -0500

    lint

--- func_func.go
+++ func_func.go
@@ -6,30 +6,35 @@ import (
 // FuncFunc is the fully general func with any number of inputs and outputs,
 // and use of a rather general function pointer that accepts and produces a slice
 // of empty interfaces (Datum) (the *Node is for tracing).
-func FuncFunc(src, dst []Edge, f func(*Node, []Datum) []Datum ) Node {
+func FuncFunc(src, dst []Edge, f func(*Node, []Datum) []Datum, anyRdy bool ) Node {
+
+	var funcRdy = func(n *Node) bool {
+		if anyRdy { return true }
+		return n.RdyAll()
+	}
+
 	var funcFire = func(n *Node) {
-		srcv := make([]Datum, 0)
+		var srcv []Datum
 		for i := range n.Srcs {
 			srcv = append(srcv, src[i].Val)
 		}
 		dstv := f(n, srcv)
-		for i := range n.Dsts {
+		for i := range dstv {
 			n.Dsts[i].Val = dstv[i]
 		}
 		
 	}
 
-
-	srcp := make([]*Edge, 0)
+	var srcp []*Edge
 	for i := range src {
 		srcp = append(srcp, &src[i])
 	}
-	dstp := make([]*Edge, 0)
+	var dstp []*Edge
 	for i := range dst {
 		dstp = append(dstp, &dst[i])
 	}
 
-	node := MakeNode("func", srcp, dstp, nil, funcFire)
+	node := MakeNode("func", srcp, dstp, funcRdy, funcFire)
 	return node
 	
 }
