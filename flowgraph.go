29fe3b8303de1297ef4bf7b6b41e481afc548624
Scott Johnston <johnston@vectaport.com>
2015-08-25 13:16:26 -0700

    Update flowgraph.go

--- flowgraph.go
+++ flowgraph.go
@@ -81,7 +81,7 @@ func MakeGraph(sze, szn int) ([]Edge,[]Node) {
 
 // ConfigByFlag initializes a standard set of command line arguments for flowgraph utilities,
 // while at the same time parsing all other flags.  Use the defaults argument to override
-// default settings for ncore, sec, trace, trsec, and chansz.
+// default settings for ncore, sec, trace, trsec, and chansz.  Use -help to see all the defaults.
 func ConfigByFlag(defaults map[string]interface{}) {
 
 	var ncoreDef interface{} = runtime.NumCPU()-1
