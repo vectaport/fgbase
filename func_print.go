c19cefbf41d229a9ed4fdc9ecf62d23925eeca93
Scott Johnston <johnston@vectaport.com>
2017-06-04 10:40:33 -0700

    new func print

---
+++ func_print.go
@@ -0,0 +1,28 @@
+package flowgraph
+
+import (		
+	"fmt"
+)      			
+
+func printFire (n *Node) {	 
+	a := n.Srcs[0] 		 
+	x := n.Dsts[0] 		 
+        form := n.Aux.(string)
+	v := a.SrcGet()
+
+	// print data string
+	_, _ = fmt.Printf(form, v)
+
+        x.DstPut(v)
+
+}
+
+// FuncPrint prints a data value using fmt.Printf
+func FuncPrint(a Edge, x Edge, format string) Node {
+
+	node := MakeNode("print", []*Edge{&a}, []*Edge{&x}, nil, printFire)
+	node.Aux = format
+	return node
+	
+}
+	
