c5a94e80638d2ab185b60d1ece890526732467ba
Scott Johnston <johnston@vectaport.com>
2017-06-08 16:30:48 -0700

    working on free running reduce

--- func_reduce.go
+++ func_reduce.go
@@ -4,15 +4,15 @@ import (
 )
 
 // FuncReduce reduces a stream of data into a single empty interface.
-func FuncReduce(a,x Edge, reducer func(n *Node, datum,collection interface{}) interface{}) Node {
+func FuncReduce(a,x Edge, reducer func(n *Node, datum,collection interface{}) interface{}, freerun bool) Node {
 
-        var reduceRdy = func (n *Node) bool {
+        var reduceFreerunRdy = func (n *Node) bool {
 		a := n.Srcs[0]
 		x := n.Dsts[0]
 		return a.SrcRdy(n) || x.DstRdy(n)
 	}
 
-	var reduceFire = func (n *Node) {
+	var reduceFreerunFire = func (n *Node) {
 	        if a.SrcRdy(n) {
   		        n.Aux = reducer(n, a.SrcGet(), n.Aux)
 		}
@@ -21,7 +21,21 @@ func FuncReduce(a,x Edge, reducer func(n *Node, datum,collection interface{}) in
 		}
 	}
 
+	var reduceSteppedFire = func (n *Node) {
+	        n.Aux = reducer(n, a.SrcGet(), n.Aux)
+	        x.DstPut(n.Aux)
+	}
 
+	var reduceRdy func (n *Node) bool = nil
+	if freerun {
+	        reduceRdy = reduceFreerunRdy
+	}
+
+        var reduceFire func (n *Node) = reduceSteppedFire
+	if freerun {
+	        reduceFire = reduceFreerunFire
+	}
+	
 	node := MakeNode("reduce", []*Edge{&a}, []*Edge{&x}, reduceRdy, reduceFire)
 	node.Aux = make([]string, 0)
 	return node
